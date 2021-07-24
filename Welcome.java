a698327655b7c3508beb7431e07fa4ce302d7714
unknown <vickyvg11@gmail.com>
2021-07-24 12:29:26 +0530

    first commit

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+class Welcome{
+
+public static void main (String args[]){
+
+System.out.println("welcome to xworkz..!!");
+
+
+}
+
+}
---
+++ Xworkz.java
@@ -0,0 +1,50 @@
+class Mobile{
+
+String name;
+String storage;
+int battery;
+String RAM;
+
+void Mindblowing(){
+System.out.println(storage+" "+name+" "+battery+" is mindblowing mobile ..!");
+
+
+}
+
+public static void main(String args[]){
+
+Mobile realme=new Mobile();
+realme.name="realme";
+realme.storage="256GB";
+realme.battery=6000;
+realme.Mindblowing();
+
+}
+
+}
+
+class Bike{
+
+
+String name;
+int CC;
+String color;
+
+
+void Racingcar(){
+System.out.println(name+" "+CC+" "+color+" is Racingcar in India..!!!");
+
+
+}
+
+public static void main(String args []){
+Bike swift=new Bike();
+swift.name="swift";
+swift.CC=1197;
+swift.color="Grey";
+swift.Racingcar();
+
+
+}
+
+}
